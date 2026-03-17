00bfc6d48d4bea27f96f51a39de464c13d69bda0
o.skvortsov <o.skvortsov@logema.org>
2026-03-17 19:30:50 +0300

    (feat) - добавлено модальное окно

--- src/04_common/components/VModal/VModal.vue
+++ src/04_common/components/VModal/VModal.vue
@@ -10,14 +10,19 @@ import {
   DialogTrigger,
 } from 'reka-ui';
 import type { ModalProps } from './VModal';
+import { VButton } from '@common/components';
+
+import CloseIcon from '~icons/custom/close';
 
 defineProps<ModalProps>();
 </script>
 
 <template>
   <DialogRoot class="modal">
-    <DialogTrigger class="modal__trigger">
-      <slot></slot>
+    <DialogTrigger class="modal__trigger" as-child>
+      <slot>
+        <v-button variant="solid" size="md"></v-button>
+      </slot>
     </DialogTrigger>
     <DialogPortal>
       <DialogOverlay class="modal__overlay" />
@@ -29,14 +34,10 @@ defineProps<ModalProps>();
           {{ description }}
         </DialogDescription>
         <slot name="content"></slot>
-        <div :style="{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }">
-          <DialogClose as-child>
-            <button class="Button green">Save changes</button>
-          </DialogClose>
-        </div>
-        <DialogClose class="IconButton" aria-label="Close">
-          <Icon icon="lucide:x" />
+        <DialogClose class="modal__close" as-child>
+          <v-button variant="ghost" size="icon-sm"><CloseIcon /></v-button>
         </DialogClose>
+        <slot name="footer"></slot>
       </DialogContent>
     </DialogPortal>
   </DialogRoot>
@@ -45,130 +46,50 @@ defineProps<ModalProps>();
 <style scoped lang="scss">
 .modal {
   &__overlay {
-    background-color: var(--black-a9);
+    background-color: color-mix(in srgb, var(--color-black) 60%, transparent);
     position: fixed;
     inset: 0;
     animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
   }
-}
 
-.DialogContent {
-  background-color: white;
-  border-radius: 6px;
-  box-shadow:
-    hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
-    hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 90vw;
-  max-width: 450px;
-  max-height: 85vh;
-  padding: 25px;
-  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
-}
-.DialogContent:focus {
-  outline: none;
-}
+  &__content {
+    background-color: var(--background);
+    border-radius: 6px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 90vw;
+    max-width: 450px;
+    max-height: 85vh;
+    padding: 25px;
+    animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
 
-.DialogTitle {
-  margin: 0;
-  font-weight: 500;
-  color: var(--mauve-12);
-  font-size: 17px;
-}
+    &:focus {
+      outline: none;
+    }
+  }
 
-.DialogDescription {
-  margin: 10px 0 20px;
-  color: var(--mauve-11);
-  font-size: 15px;
-  line-height: 1.5;
-}
+  &__title {
+    margin: 0;
+    font-weight: 500;
+    color: var(--foreground);
+    font-family: Geologica, sans-serif;
+    font-size: 17px;
+  }
 
-.Button {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  padding: 0 15px;
-  font-size: 15px;
-  line-height: 1;
-  font-weight: 500;
-  height: 35px;
-}
-.Button.grass {
-  background-color: white;
-  color: var(--grass-11);
-  box-shadow: 0 2px 10px var(--black-a7);
-}
-.Button.grass:hover {
-  background-color: var(--mauve-3);
-}
-.Button.grass:focus {
-  box-shadow: 0 0 0 2px black;
-}
-.Button.green {
-  background-color: var(--green-4);
-  color: var(--green-11);
-}
-.Button.green:hover {
-  background-color: var(--green-5);
-}
-.Button.green:focus {
-  box-shadow: 0 0 0 2px var(--green-7);
-}
+  &__description {
+    margin: 10px 0 20px;
+    color: var(--mauve-11);
+    font-size: 15px;
+    line-height: 1.5;
+  }
 
-.IconButton {
-  font-family: inherit;
-  border-radius: 100%;
-  height: 25px;
-  width: 25px;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  color: var(--grass-11);
-  position: absolute;
-  top: 10px;
-  right: 10px;
-}
-.IconButton:hover {
-  background-color: var(--grass-4);
-}
-.IconButton:focus {
-  box-shadow: 0 0 0 2px var(--grass-7);
-}
-
-.Fieldset {
-  display: flex;
-  gap: 20px;
-  align-items: center;
-  margin-bottom: 15px;
-}
-
-.Label {
-  font-size: 15px;
-  color: var(--grass-11);
-  width: 90px;
-  text-align: right;
-}
-
-.Input {
-  width: 100%;
-  flex: 1;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  padding: 0 10px;
-  font-size: 15px;
-  line-height: 1;
-  color: var(--grass-11);
-  box-shadow: 0 0 0 1px var(--grass-7);
-  height: 35px;
-}
-.Input:focus {
-  box-shadow: 0 0 0 2px var(--grass-8);
+  &__close {
+    position: absolute;
+    top: rem(10);
+    right: rem(10);
+  }
 }
 
 @keyframes overlayShow {
